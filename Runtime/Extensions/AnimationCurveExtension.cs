6eb3f45156185813ec7a3474fd1f6b4ccfeb462a
Hyago Oliveira <hyagogow@gmail.com>
2026-07-13 12:12:12 -0300

    Add AnimationCurveExtension

---
+++ Runtime/Extensions/AnimationCurveExtension.cs
@@ -0,0 +1,299 @@
+using UnityEngine;
+
+namespace ActionCode.AnimationSystem
+{
+    /// <summary>
+    /// Extension class for AnimationCurves.
+    /// </summary>
+    public static class AnimationCurveExtension
+    {
+        /// <summary>
+        /// Reshapes the animation curve into a preset shape based on the provided <see cref="AnimationEase"/>.
+        /// </summary>
+        /// <param name="curve"><inheritdoc cref="SetAsSineWave(AnimationCurve, float, int)" path="/param[@name='curve']"/></param>
+        /// <param name="ease">The predefined ease style type to apply to the curve.</param>
+        public static void SetAs(this AnimationCurve curve, AnimationEase ease)
+        {
+            switch (ease)
+            {
+                case AnimationEase.Sine: curve.SetAsSineWave(); break;
+                case AnimationEase.Cosine: curve.SetAsCosineWave(); break;
+                case AnimationEase.Elastic: curve.SetAsElastic(); break;
+                case AnimationEase.ElasticIn: curve.SetAsElasticIn(); break;
+                case AnimationEase.ElasticOut: curve.SetAsElasticOut(); break;
+                case AnimationEase.Bounce: curve.SetAsBounce(); break;
+                case AnimationEase.BounceIn: curve.SetAsBounceIn(); break;
+                case AnimationEase.BounceOut: curve.SetAsBounceOut(); break;
+            }
+        }
+
+        #region SINE AND COSINE
+        /// <summary>
+        /// Reshapes the animation curve into a Sine wave.
+        /// </summary>
+        /// <param name="curve">The Target AnimationCurve instance.</param>
+        /// <param name="frequency">The number of wave cycles to fit within the curve timeline.</param>
+        /// <param name="resolution">The total number of keyframes used to shape the wave.</param>
+        public static void SetAsSineWave(this AnimationCurve curve, float frequency = 1f, int resolution = 30)
+        {
+            if (curve == null) return;
+            var keys = new Keyframe[resolution];
+
+            for (var i = 0; i < resolution; i++)
+            {
+                var time = (float)i / (resolution - 1);
+                var value = Mathf.Sin(time * frequency * 2f * Mathf.PI);
+                keys[i] = new Keyframe(time, value);
+            }
+
+            curve.keys = keys;
+
+            for (int i = 0; i < resolution; i++)
+            {
+                curve.SmoothTangents(i, 0f);
+            }
+        }
+
+        /// <summary>
+        /// Reshapes the animation curve into a Cosine wave.
+        /// </summary>
+        /// <param name="curve"><inheritdoc cref="SetAsSineWave(AnimationCurve, float, int)" path="/param[@name='curve']"/></param>
+        /// <param name="frequency"><inheritdoc cref="SetAsSineWave(AnimationCurve, float, int)" path="/param[@name='frequency']"/></param>
+        /// <param name="resolution"><inheritdoc cref="SetAsSineWave(AnimationCurve, float, int)" path="/param[@name='resolution']"/></param>
+        public static void SetAsCosineWave(this AnimationCurve curve, float frequency = 1f, int resolution = 30)
+        {
+            if (curve == null) return;
+            var keys = new Keyframe[resolution];
+
+            for (var i = 0; i < resolution; i++)
+            {
+                var time = (float)i / (resolution - 1);
+                var value = Mathf.Cos(time * frequency * 2f * Mathf.PI);
+                keys[i] = new Keyframe(time, value);
+            }
+
+            curve.keys = keys;
+
+            for (var i = 0; i < resolution; i++)
+            {
+                curve.SmoothTangents(i, 0f);
+            }
+        }
+        #endregion
+
+        #region ELASTIC
+        /// <summary>
+        /// Reshapes the animation curve into an Elastic In wave (EaseInElastic).
+        /// </summary>
+        /// <param name="curve"><inheritdoc cref="SetAsSineWave(AnimationCurve, float, int)" path="/param[@name='curve']"/></param>
+        /// <param name="resolution"><inheritdoc cref="SetAsSineWave(AnimationCurve, float, int)" path="/param[@name='resolution']"/></param>
+        public static void SetAsElasticIn(this AnimationCurve curve, int resolution = 60)
+        {
+            if (curve == null) return;
+
+            var keys = new Keyframe[resolution];
+            const float c4 = (2f * Mathf.PI) / 3f;
+
+            for (var i = 0; i < resolution; i++)
+            {
+                var time = (float)i / (resolution - 1);
+                float value;
+
+                if (time <= 0f) value = 0f;
+                else if (time >= 1f) value = 1f;
+                else value = -Mathf.Pow(2f, 10f * time - 10f) * Mathf.Sin((time * 10f - 10.75f) * c4);
+
+                keys[i] = new Keyframe(time, value);
+            }
+
+            curve.keys = keys;
+
+            for (var i = 0; i < resolution; i++)
+            {
+                curve.SmoothTangents(i, 0f);
+            }
+        }
+
+        /// <summary>
+        /// Reshapes the animation curve into an Elastic Out wave (EaseOutElastic).
+        /// </summary>
+        /// <param name="curve"><inheritdoc cref="SetAsSineWave(AnimationCurve, float, int)" path="/param[@name='curve']"/></param>
+        /// <param name="resolution"><inheritdoc cref="SetAsSineWave(AnimationCurve, float, int)" path="/param[@name='resolution']"/></param>
+        public static void SetAsElasticOut(this AnimationCurve curve, int resolution = 60)
+        {
+            if (curve == null) return;
+
+            var keys = new Keyframe[resolution];
+            const float c4 = (2f * Mathf.PI) / 3f;
+
+            for (var i = 0; i < resolution; i++)
+            {
+                var time = (float)i / (resolution - 1);
+                float value;
+
+                if (time <= 0f) value = 0f;
+                else if (time >= 1f) value = 1f;
+                else
+                {
+                    // Standard EaseOutElastic formula: 2^(-10x) * sin((x * 10 - 0.75) * c4) + 1
+                    value = Mathf.Pow(2f, -10f * time) * Mathf.Sin((time * 10f - 0.75f) * c4) + 1f;
+                }
+
+                keys[i] = new Keyframe(time, value);
+            }
+
+            curve.keys = keys;
+
+            for (var i = 0; i < resolution; i++)
+            {
+                curve.SmoothTangents(i, 0f);
+            }
+        }
+
+        /// <summary>
+        /// Reshapes the animation curve into an Elastic In Out wave (EaseInOutElastic).
+        /// </summary>
+        /// <param name="curve"><inheritdoc cref="SetAsSineWave(AnimationCurve, float, int)" path="/param[@name='curve']"/></param>
+        /// <param name="resolution"><inheritdoc cref="SetAsSineWave(AnimationCurve, float, int)" path="/param[@name='resolution']"/></param>
+        public static void SetAsElastic(this AnimationCurve curve, int resolution = 80)
+        {
+            if (curve == null) return;
+
+            var keys = new Keyframe[resolution];
+            const float c5 = (2f * Mathf.PI) / 4.5f;
+
+            for (var i = 0; i < resolution; i++)
+            {
+                var time = (float)i / (resolution - 1);
+                float value;
+
+                if (time <= 0f) value = 0f;
+                else if (time >= 1f) value = 1f;
+                else if (time < 0.5f) value = -(Mathf.Pow(2f, 20f * time - 10f) * Mathf.Sin((20f * time - 11.125f) * c5)) / 2f;
+                else value = (Mathf.Pow(2f, -20f * time + 10f) * Mathf.Sin((20f * time - 11.125f) * c5)) / 2f + 1f;
+
+                keys[i] = new Keyframe(time, value);
+            }
+
+            curve.keys = keys;
+
+            for (var i = 0; i < resolution; i++)
+            {
+                curve.SmoothTangents(i, 0f);
+            }
+        }
+        #endregion
+
+        #region BOUNCE
+        /// <summary>
+        /// Reshapes the animation curve into a Bounce In wave (EaseInBounce).
+        /// </summary>
+        /// <param name="curve"><inheritdoc cref="SetAsSineWave(AnimationCurve, float, int)" path="/param[@name='curve']"/></param>
+        /// <param name="resolution"><inheritdoc cref="SetAsSineWave(AnimationCurve, float, int)" path="/param[@name='resolution']"/></param>
+        public static void SetAsBounceIn(this AnimationCurve curve, int resolution = 60)
+        {
+            if (curve == null) return;
+            var keys = new Keyframe[resolution];
+
+            for (var i = 0; i < resolution; i++)
+            {
+                var time = (float)i / (resolution - 1);
+                var bounceTime = 1f - time;
+                float value;
+
+                if (bounceTime < 1f / 2.75f) value = 7.5625f * bounceTime * bounceTime;
+                else if (bounceTime < 2f / 2.75f) value = 7.5625f * (bounceTime -= 1.5f / 2.75f) * bounceTime + 0.75f;
+                else if (bounceTime < 2.5f / 2.75f) value = 7.5625f * (bounceTime -= 2.25f / 2.75f) * bounceTime + 0.9375f;
+                else value = 7.5625f * (bounceTime -= 2.625f / 2.75f) * bounceTime + 0.984375f;
+
+                keys[i] = new Keyframe(time, 1f - value);
+            }
+
+            curve.keys = keys;
+
+            for (var i = 0; i < resolution; i++)
+            {
+                curve.SmoothTangents(i, 0f);
+            }
+        }
+
+        /// <summary>
+        /// Reshapes the animation curve into a Bounce Out wave (EaseOutBounce).
+        /// </summary>
+        /// <param name="curve"><inheritdoc cref="SetAsSineWave(AnimationCurve, float, int)" path="/param[@name='curve']"/></param>
+        /// <param name="resolution"><inheritdoc cref="SetAsSineWave(AnimationCurve, float, int)" path="/param[@name='resolution']"/></param>
+        public static void SetAsBounceOut(this AnimationCurve curve, int resolution = 60)
+        {
+            if (curve == null) return;
+            var keys = new Keyframe[resolution];
+
+            for (var i = 0; i < resolution; i++)
+            {
+                var time = (float)i / (resolution - 1);
+                float value;
+
+                if (time < 1f / 2.75f) value = 7.5625f * time * time;
+                else if (time < 2f / 2.75f) value = 7.5625f * (time -= 1.5f / 2.75f) * time + 0.75f;
+                else if (time < 2.5f / 2.75f) value = 7.5625f * (time -= 2.25f / 2.75f) * time + 0.9375f;
+                else value = 7.5625f * (time -= 2.625f / 2.75f) * time + 0.984375f;
+
+                keys[i] = new Keyframe((float)i / (resolution - 1), value);
+            }
+
+            curve.keys = keys;
+
+            for (var i = 0; i < resolution; i++)
+            {
+                curve.SmoothTangents(i, 0f);
+            }
+        }
+
+        /// <summary>
+        /// Reshapes the animation curve into a Bounce In Out wave (EaseInOutBounce).
+        /// </summary>
+        /// <param name="curve"><inheritdoc cref="SetAsSineWave(AnimationCurve, float, int)" path="/param[@name='curve']"/></param>
+        /// <param name="resolution"><inheritdoc cref="SetAsSineWave(AnimationCurve, float, int)" path="/param[@name='resolution']"/></param>
+        public static void SetAsBounce(this AnimationCurve curve, int resolution = 80)
+        {
+            if (curve == null) return;
+            var keys = new Keyframe[resolution];
+
+            for (var i = 0; i < resolution; i++)
+            {
+                var time = (float)i / (resolution - 1);
+                float value;
+
+                if (time < 0.5f)
+                {
+                    var bounceTime = 1f - (time * 2f);
+                    if (bounceTime < 1f / 2.75f) value = 7.5625f * bounceTime * bounceTime;
+                    else if (bounceTime < 2f / 2.75f) value = 7.5625f * (bounceTime -= 1.5f / 2.75f) * bounceTime + 0.75f;
+                    else if (bounceTime < 2.5f / 2.75f) value = 7.5625f * (bounceTime -= 2.25f / 2.75f) * bounceTime + 0.9375f;
+                    else value = 7.5625f * (bounceTime -= 2.625f / 2.75f) * bounceTime + 0.984375f;
+
+                    value = (1f - value) * 0.5f;
+                }
+                else
+                {
+                    var bounceTime = time * 2f - 1f;
+                    if (bounceTime < 1f / 2.75f) value = 7.5625f * bounceTime * bounceTime;
+                    else if (bounceTime < 2f / 2.75f) value = 7.5625f * (bounceTime -= 1.5f / 2.75f) * bounceTime + 0.75f;
+                    else if (bounceTime < 2.5f / 2.75f) value = 7.5625f * (bounceTime -= 2.25f / 2.75f) * bounceTime + 0.9375f;
+                    else value = 7.5625f * (bounceTime -= 2.625f / 2.75f) * bounceTime + 0.984375f;
+
+                    value = value * 0.5f + 0.5f;
+                }
+
+                keys[i] = new Keyframe(time, value);
+            }
+
+            curve.keys = keys;
+
+            for (var i = 0; i < resolution; i++)
+            {
+                curve.SmoothTangents(i, 0f);
+            }
+        }
+        #endregion
+    }
+}
---
+++ Runtime/Extensions/AnimationEase.cs
@@ -0,0 +1,16 @@
+namespace ActionCode.AnimationSystem
+{
+    public enum AnimationEase
+    {
+        Sine,
+        Cosine,
+
+        Elastic,
+        ElasticIn,
+        ElasticOut,
+
+        Bounce,
+        BounceIn,
+        BounceOut
+    }
+}
