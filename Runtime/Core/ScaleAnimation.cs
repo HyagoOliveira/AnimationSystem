b62b95ed11659429991171109004bcd1a43a656b
Hyago Oliveira <hyagogow@gmail.com>
2026-07-13 10:05:10 -0300

    Add ScaleAnimation

---
+++ Runtime/Core/ScaleAnimation.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+namespace ActionCode.AnimationSystem
+{
+    /// <summary>
+    /// Scale animation for the local transform.
+    /// <para>
+    /// Use the <see cref="scaleCurve"/> curve to animate the scale.
+    /// </para>
+    /// </summary>
+    public sealed class ScaleAnimation : AbstractCoreAnimation
+    {
+        [Space]
+        [SerializeField, Tooltip("The curve driving the scale animation.")]
+        private AnimationCurve scaleCurve;
+
+        protected override void UpdateAnimation(float time)
+        {
+            base.UpdateAnimation(time);
+            var scale = scaleCurve.Evaluate(CurrentTime);
+
+            transform.localScale = Vector3.one * scale;
+            CheckStopCondition(scaleCurve);
+        }
+    }
+}
