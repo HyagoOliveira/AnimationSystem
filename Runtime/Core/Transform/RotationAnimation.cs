49425f88f23a4537f5ea43f0ddeee9639b144c63
Hyago Oliveira <hyagogow@gmail.com>
2026-07-14 09:30:05 -0300

    Add axisCurve

--- Runtime/Core/Transform/RotationAnimation.cs
+++ Runtime/Core/Transform/RotationAnimation.cs
@@ -1,3 +1,4 @@
+using OneM.Attributes;
 using UnityEngine;
 
 namespace ActionCode.AnimationSystem
@@ -8,16 +9,40 @@ namespace ActionCode.AnimationSystem
     [AddComponentMenu("Animation/Transform/Rotation")]
     public sealed class RotationAnimation : AbstractCoreAnimation
     {
+        [Tooltip("Wether to use animation curves for every axis.")]
+        public bool useAnimationCurves;
+
         [Tooltip("Whether to rotate locally or relative to the Scene in world space.")]
+        [ShowIf(nameof(useAnimationCurves), operatorType: LogicalOperatorType.Equals, value: false)]
         public Space relation = Space.Self;
         [Tooltip("The axis used to rotate.")]
+        [ShowIf(nameof(useAnimationCurves), operatorType: LogicalOperatorType.Equals, value: false)]
         public Vector3 axisSpeed = Vector3.up;
+
+        [Tooltip("The animation curves for every axis")]
+        [ShowIf(nameof(useAnimationCurves))]
+        public Vector3Curve axisCurve = new();
+
+        protected override void Reset()
+        {
+            base.Reset();
+            axisCurve.Reset();
+        }
 
         protected override void UpdateAnimation()
         {
             base.UpdateAnimation();
-            var velocity = GetDeltaTime() * speed * axisSpeed;
-            transform.Rotate(velocity, relation);
+
+            if (useAnimationCurves)
+            {
+                var rotation = axisCurve.Evaluate(CurrentTime);
+                transform.localEulerAngles = rotation;
+            }
+            else
+            {
+                var velocity = GetDeltaTime() * speed * axisSpeed;
+                transform.Rotate(velocity, relation);
+            }
         }
     }
 }
